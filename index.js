f0d5afb4a70d9656272ae83b02018ef0ab9d9093
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 17:38:02 +0200

    consoleEvents and commandLogging

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
-const { BOT_TOKEN } = process.env;
+const { BOT_TOKEN, ENVIRONMENT } = process.env;
 const { Client } = require('klasa');
 
 new Client({
 	fetchAllMembers: false,
 	prefix: ['leaks', 'pls', 'leo pls', 'kenn pls', 'views pls', 'emrys pls', 'krusher pls'],
 	commandEditing: true,
+	commandLogging: ENVIRONMENT == 'dev',
 	typing: true,
-	production: true,
+	production: ENVIRONMENT == 'prod',
 	prefixCaseInsensitive: true,
+	consoleEvents: {
+		debug: ENVIRONMENT == 'dev',
+		error: true,
+		log: true,
+		verbose: ENVIRONMENT == 'dev',
+		warn: true,
+		wtf: true
+	},
 	readyMessage: client => `Successfully initialized. Ready to serve ${client.guilds.cache.size} guilds.`
 }).login(BOT_TOKEN);
