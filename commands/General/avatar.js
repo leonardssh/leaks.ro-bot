c6662507062c8240259e1df41524118549aa46af
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 19:21:42 +0200

    update avatar command

--- commands/General/avatar.js
+++ commands/General/avatar.js
@@ -6,8 +6,7 @@ module.exports = class extends Command {
 		super(...args, {
 			guarded: true,
 			description: 'Displays the avatar of the specified user.',
-			requiredPermissions: ['EMBED_LINKS'],
-			category: 'General'
+			requiredPermissions: ['EMBED_LINKS']
 		});
 	}
 
