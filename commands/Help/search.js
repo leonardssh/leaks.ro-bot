c66642f0fc5bd0781bf3deefa513804eeeb23fc9
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 19:17:45 +0200

    search command

---
+++ commands/Help/search.js
@@ -0,0 +1,37 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+	constructor(...args) {
+		super(...args, {
+			guarded: true,
+			description: 'Look for commands by keywords.',
+			usage: '<Keyword:str>',
+			requiredPermissions: ['EMBED_LINKS']
+		});
+	}
+
+	async run(message, [command]) {
+		command = command.toLowerCase();
+
+		let commands = this.client.commands.map(c => c.name.toLowerCase()).filter(c => c.includes(command));
+
+		if (!commands.length) {
+			return message.sendEmbed(
+				new MessageEmbed()
+					.setColor('#e74c3c')
+					.setDescription(
+						`<:valet_nope:716348860389261395> ${message.author}, I'm sorry, but I didn't find any command containing these keywords.`
+					)
+			);
+		}
+
+		message.sendEmbed(
+			new MessageEmbed()
+				.setColor('#008dff')
+				.setDescription(`<:valet_yeah:716348838289342496> I found ${commands.length} commands containing **${command}**.`)
+				.addField('Commands:', `\`\`\`diff\n${commands.join('\n')}\`\`\``)
+				.addField(':grey_question: Want more details?', `Use \`help [Command]\` for more details about a specific command.`)
+		);
+	}
+};
