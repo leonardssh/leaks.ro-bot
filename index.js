047c23189e2b99949e83780d30954c11582d58f8
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 21:46:07 +0200

    some changes

--- index.js
+++ index.js
@@ -1,45 +1,9 @@
 require('dotenv').config();
+require('./prototypes');
 
-const { BOT_TOKEN, ENVIRONMENT } = process.env;
-const { Client, Schema } = require('klasa');
+const { Client } = require('klasa');
 
-const client = new Client({
-	presence: {
-		activity: {
-			name: 'leaks help | @leaks',
-			type: 'LISTENING'
-		}
-	},
-	fetchAllMembers: false,
-	prefix: ['leaks', 'pls', 'leo pls', 'kenn pls', 'views pls', 'emrys pls', 'krusher pls'],
-	commandEditing: true,
-	commandLogging: ENVIRONMENT == 'dev',
-	typing: true,
-	production: ENVIRONMENT == 'prod',
-	prefixCaseInsensitive: true,
-	consoleEvents: {
-		debug: ENVIRONMENT == 'dev',
-		error: true,
-		log: true,
-		verbose: ENVIRONMENT == 'dev',
-		warn: true,
-		wtf: true
-	},
-	disabledCorePieces: ['commands', 'languages'],
-	gateways: {
-		guilds: {
-			schema: new Schema()
-				.add('disabledCommands', 'command', {
-					array: true,
-					filter: (client, command, piece, language) => {
-						if (command.guarded) throw language.get('COMMAND_CONF_GUARDED', command.name);
-					}
-				})
-				.add('antiInvite', 'boolean', { default: false })
-				.add('muted', 'role')
-		}
-	},
-	readyMessage: client => `Successfully initialized. Ready to serve ${client.guilds.cache.size} guilds.`
-});
+const options = require('./utils/options');
+const client = new Client(options);
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
