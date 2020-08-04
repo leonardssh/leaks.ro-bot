98ec84a776400bfa5eecab5899b1a02565044ad0
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 21:46:23 +0200

    gateways

---
+++ utils/gateways.js
@@ -0,0 +1,18 @@
+const { Schema } = require('klasa');
+
+module.exports = {
+	gateways: {
+		guilds: {
+			schema: new Schema()
+				.add('disabledCommands', 'command', {
+					array: true,
+					filter: (client, command, piece, language) => {
+						if (command.guarded) throw language.get('COMMAND_CONF_GUARDED', command.name);
+					}
+				})
+				.add('antiInvite', 'boolean', { default: false })
+				.add('muted', 'role')
+				.add('moderator', 'role')
+		}
+	}
+};
