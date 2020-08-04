d768fe2697ed93a45267100b4617bd1c9b73afa5
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 19:17:38 +0200

    update help command

--- commands/Help/help.js
+++ commands/Help/help.js
@@ -1,17 +1,13 @@
 const {
 	Command,
-	RichDisplay,
 	util: { isFunction }
 } = require('klasa');
-const { MessageEmbed, Permissions } = require('discord.js');
-
-const PERMISSIONS_RICHDISPLAY = new Permissions([Permissions.FLAGS.MANAGE_MESSAGES, Permissions.FLAGS.ADD_REACTIONS]);
-const time = 1000 * 60 * 3;
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
 
 module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
-			aliases: ['commands', 'cmd', 'cmds'],
 			guarded: true,
 			description: 'Shows you the help center.',
 			usage: '(Command:command)',
@@ -25,13 +21,11 @@ module.exports = class extends Command {
 
 			return this.client.arguments.get('command').run(arg, possible, message);
 		});
-
-		this.handlers = new Map();
 	}
 
 	async run(message, [command]) {
 		if (command) {
-			const { name, description, category, usage, aliases, requiredPermissions, permissionLevel } = command;
+			const { name, description, category, usage, aliases, requiredPermissions } = command;
 
 			return message.sendEmbed(
 				new MessageEmbed()
@@ -40,7 +34,6 @@ module.exports = class extends Command {
 					.setDescription(isFunction(description) ? description(message.language) : description)
 					.addField('**__CATEGORY__**', category)
 					.addField('**__USAGE__**', `\`\`\`\n${usage.fullUsage(message)}\`\`\``)
-					.addField('**__ALIASES__**', `\`\`\`\n${aliases.join(', ') || 'No aliases set.'}\`\`\``)
 					.addField(
 						'**__CLIENT PERMISSIONS__**',
 						`\`\`\`\n${
@@ -54,72 +47,22 @@ module.exports = class extends Command {
 			);
 		}
 
-		if (!('all' in message.flags) && message.guild && message.channel.permissionsFor(this.client.user).has(PERMISSIONS_RICHDISPLAY)) {
-			const previousHandler = this.handlers.get(message.author.id);
-			if (previousHandler) {
-				previousHandler.stop();
-			}
-
-			const handler = await (await this.buildDisplay(message)).run(await message.send('Loading Commands...'), {
-				filter: (reaction, user) => user.id === message.author.id,
-				time
-			});
-
-			handler.on('end', () => this.handlers.delete(message.author.id));
-			this.handlers.set(message.author.id, handler);
-			return handler;
-		}
-	}
-
-	async buildDisplay(message) {
-		const commands = await this._fetchCommands(message);
-		const display = new RichDisplay().setFooterPrefix(`Total Commands: ${this.client.commands.size} • Page: `);
-		const { prefix } = message.guildSettings;
-
-		for (const [category, list] of commands) {
-			display.addPage(
-				new MessageEmbed()
-					.setColor('#008dff')
-					.setDescription(
-						`**__PREFIXES__**\n${prefix
-							.map(pre => `\`${pre}\``)
-							.join(', ')}\n\n **__${category.toUpperCase()} COMMANDS__**\n${list
-							.map(this.formatCommand.bind(this, message, true))
-							.join('\n')}`
-					)
-			);
-		}
-
-		return display;
-	}
-
-	formatCommand(message, richDisplay, command) {
-		const description = isFunction(command.description) ? command.description(message.language) : command.description;
-		return richDisplay ? `\`${command.name}\` - ${description}` : `\`${command.name}\` - ${description}`;
-	}
-
-	async _fetchCommands(message) {
-		const run = this.client.inhibitors.run.bind(this.client.inhibitors, message);
-		const commands = new Map();
-
-		await Promise.all(
-			this.client.commands.map(command =>
-				run(command, true)
-					.then(() => {
-						const category = commands.get(command.category);
-
-						if (category) {
-							category.push(command);
-						} else {
-							commands.set(command.category, [command]);
-						}
-					})
-					.catch(() => {
-						// noop
-					})
-			)
+		message.sendEmbed(
+			new MessageEmbed()
+				.setTitle(`Help Center`)
+				.setColor('#008dff')
+				.setThumbnail(message.author.avatarURL({ dynamic: true, size: 2048, format: 'png' }))
+				.setDescription(
+					stripIndents`Hi ${message.author}, nice to meet you! :wave:\n\
+						Looks like you need help, let me help you! :hugging:\n\n\
+						`
+				)
+				.addField(`:grey_question: Want to know what my prefixes are?`, 'Mention me!')
+				.addField(`:grey_question: Want to know all the commands I've? :face_with_monocle:`, `Use the \`commands\` command!`)
+				.addField(`:grey_question: Want to know more details about a specific command?`, 'Use the `help [Command]` command!')
+				.addField(":grey_question: Can't find a command? Search for it using a keyword!", `Use the \`search [Keyword]\` command!`)
+				.setFooter(this.client.user.username, this.client.user.avatarURL({ dynamic: true, size: 2048, format: 'png' }))
+				.setTimestamp()
 		);
-
-		return commands;
 	}
 };
