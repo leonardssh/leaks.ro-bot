28ec7c90aa060b9e74c0045dde45afaa301a153d
LeonardSSH <leonardssh22@gmail.com>
2020-08-04 19:05:49 +0200

    avatar command

---
+++ commands/General/avatar.js
@@ -0,0 +1,32 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+	constructor(...args) {
+		super(...args, {
+			guarded: true,
+			description: 'Displays the avatar of the specified user.',
+			requiredPermissions: ['EMBED_LINKS'],
+			category: 'General'
+		});
+	}
+
+	async run(message) {
+		let mentionedUser = message.mentions.users.first() || message.author;
+
+		await message.send('Generating avatar...');
+		message.sendEmbed(
+			new MessageEmbed()
+				.setImage(mentionedUser.avatarURL({ dynamic: true, size: 2048, format: 'png' }))
+				.setColor('#008dff')
+				.setTitle(`${mentionedUser.username}'s Avatar`)
+				.setDescription(
+					`[click here for download](${mentionedUser.avatarURL({
+						dynamic: true,
+						size: 2048,
+						format: 'png'
+					})})`
+				)
+		);
+	}
+};
