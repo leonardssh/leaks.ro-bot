b9d0fb4cb279a64065e71e051b977980615e6ae3
LeonardSSH <leonardssh22@gmail.com>
2020-08-05 19:53:12 +0200

    update ban command

--- commands/Moderator/ban.js
+++ commands/Moderator/ban.js
@@ -21,11 +21,11 @@ module.exports = class extends Command {
 		const member = await msg.guild.members.fetch(user).catch(() => null);
 
 		if (!member) {
-			return this.constructor.pushError(msg, `${msg.author}, this user is no longer on the server.`);
+			throw `${msg.author}, this user is no longer on the server.`;
 		} else if (member.roles.highest.position >= msg.member.roles.highest.position) {
-			return this.constructor.pushError(msg, `${msg.author}, you may not execute this command on this member.`);
+			throw `${msg.author}, you may not execute this command on this member.`;
 		} else if (member.bannable === false) {
-			return this.constructor.pushError(msg, `${msg.author}, I am not able to ban this member, sorry.`);
+			throw `${msg.author}, I am not able to ban this member, sorry.`;
 		}
 
 		await msg.guild.members.ban(user, { reason, days });
@@ -40,19 +40,14 @@ module.exports = class extends Command {
 			log.send();
 		}
 
-		return this.constructor.pushSuccess(
-			msg,
-			`**${msg.author.tag}** banned **${user.tag}**${
-				days ? ` and removes **${days}** days of their message history` : ''
-			}, reason: **${reason || 'No reason.'}**`
+		return msg.sendEmbed(
+			new MessageEmbed()
+				.setColor('#43b581')
+				.setDescription(
+					`<:valet_yeah:716348838289342496> **${msg.author.tag}** banned **${user.tag}**${
+						days ? ` and removes **${days}** days of their message history` : ''
+					}, reason: **${reason || 'No reason.'}**`
+				)
 		);
-	}
-
-	static pushError(msg, errorMsg) {
-		return msg.sendEmbed(new MessageEmbed().setColor('#e74c3c').setDescription(`<:valet_nope:716348860389261395> ${errorMsg}`));
-	}
-
-	static pushSuccess(msg, successMsg) {
-		return msg.sendEmbed(new MessageEmbed().setColor('#43b581').setDescription(`<:valet_yeah:716348838289342496> ${successMsg}`));
 	}
 };
