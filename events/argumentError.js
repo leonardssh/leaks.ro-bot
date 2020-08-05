43906fd7929ab4d30b586fc721d40ee1c4006e34
LeonardSSH <leonardssh22@gmail.com>
2020-08-05 19:42:13 +0200

    update argumentError event

--- events/argumentError.js
+++ events/argumentError.js
@@ -1,7 +1,20 @@
-const { Event } = require('klasa');
+const {
+	Event,
+	util: { isFunction }
+} = require('klasa');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Event {
 	run(message, command, params, error) {
-		message.sendMessage(error).catch(err => this.client.emit('wtf', err));
+		message
+			.sendEmbed(
+				new MessageEmbed()
+					.setColor('#e74c3c')
+					.setTitle('Invalid Command Syntax')
+					.setDescription(isFunction(command.description) ? description(message.language) : command.description)
+					.addField('**Usage**', `\`\`\`js\n${command.usage.fullUsage(message)}\`\`\``)
+					.addField('**Need help?**', `Use the \`help ${command.name}\` command for more details about this command.`)
+			)
+			.catch(err => this.client.emit('wtf', err));
 	}
 };
