36c992d41cd04ee31c934f84591e4b40ddf5d60a
LeonardSSH <leonardssh22@gmail.com>
2020-08-05 11:16:46 +0200

    update enable command

--- commands/Admin/enable.js
+++ commands/Admin/enable.js
@@ -6,7 +6,8 @@ module.exports = class extends Command {
 			permissionLevel: 10,
 			guarded: true,
 			description: language => language.get('COMMAND_ENABLE_DESCRIPTION'),
-			usage: '<Piece:piece>'
+			usage: '<Piece:piece>',
+			runIn: ['text', 'dm']
 		});
 	}
 
