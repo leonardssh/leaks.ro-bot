5d3a4d00208b2a089d8e755190cb7845dd0694a2
LeonardSSH <leonardssh22@gmail.com>
2020-08-06 12:48:52 +0200

    requiredSettings for unmute command

--- commands/Moderator/unmute.js
+++ commands/Moderator/unmute.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			permissionLevel: 6,
 			requiredPermissions: ['MANAGE_ROLES'],
 			runIn: ['text'],
-			requiredSettings: ['modlog'],
+			requiredSettings: ['channels.modlog'],
 			description: 'Unmutes a mentioned user.',
 			usage: '<Member:member> [Reason:...string]',
 			usageDelim: ' '
