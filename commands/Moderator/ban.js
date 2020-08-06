551e87411005e7091bce611b7210d3b79e4ff291
LeonardSSH <leonardssh22@gmail.com>
2020-08-06 12:04:49 +0200

    update ban command

--- commands/Moderator/ban.js
+++ commands/Moderator/ban.js
@@ -15,36 +15,36 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(msg, [user, days = 0, ...reason]) {
+	async run(message, [user, days = 0, ...reason]) {
 		reason = reason.length > 0 ? reason.join(' ') : null;
 
-		const member = await msg.guild.members.fetch(user).catch(() => null);
+		const member = await message.guild.members.fetch(user).catch(() => null);
 
 		if (!member) {
-			throw `${msg.author}, this user is no longer on the server.`;
-		} else if (member.roles.highest.position >= msg.member.roles.highest.position) {
-			throw `${msg.author}, you may not execute this command on this member.`;
+			throw `${message.author}, this user is no longer on the server.`;
+		} else if (member.roles.highest.position >= message.member.roles.highest.position) {
+			throw `${message.author}, you may not execute this command on this member.`;
 		} else if (member.bannable === false) {
-			throw `${msg.author}, I am not able to ban this member, sorry.`;
+			throw `${message.author}, I am not able to ban this member, sorry.`;
 		}
 
-		await msg.guild.members.ban(user, { reason, days });
+		await message.guild.members.ban(user, { reason, days });
 
-		if (msg.guild.settings.channels.modlog) {
-			const log = new ModLog(msg.guild);
+		if (message.guild.settings.channels.modlog) {
+			const log = new ModLog(message.guild);
 
 			log.type = 'ban';
-			log.moderator = msg.author;
+			log.moderator = message.author;
 			log.user = user;
 			log.reason = reason;
 			log.send();
 		}
 
-		return msg.sendEmbed(
+		return message.sendEmbed(
 			new MessageEmbed()
 				.setColor('#43b581')
 				.setDescription(
-					`<:valet_yeah:716348838289342496> **${msg.author.tag}** banned **${user.tag}**${
+					`<:valet_yeah:716348838289342496> **${message.author.tag}** banned **${user.tag}**${
 						days ? ` and removes **${days}** days of their message history` : ''
 					}, reason: **${reason || 'No reason.'}**`
 				)
